b11a31700e912b3302ed0e752d16cb440c6a2214
Hazael Ruiz <ruizdiazhazael.education@gmail.com>
2024-12-09 01:05:09 -0600

    Add img folder and update CartWidget and index.js

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CartWidget = () => {
     return (
         <div className="cart-widget">
-            <img src="./img/cart-icon.png" alt="Carrito de Compras" className="cart-icon" />
+            <img src="../img/cart-icon.png" alt="Carrito de Compras" className="cart-icon" />
             <span className="cart-count">0</span>
         </div>
     );
